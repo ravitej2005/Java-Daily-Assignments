bad07dc86e6f0ec76b9d24c3392b6d0b4b5ee528
Ravitej Mulay <ravitejmulay93@gmail.com>
2024-08-26 19:39:04 +0530

    Day 1 commits

---
+++ Day 1/exercise1.java
@@ -0,0 +1,17 @@
+class exercise1 {
+  public static void main(String[] args){
+    // int a = 0;
+    int a = 1;
+    // int a = -1;
+    if (a<0) {
+      System.out.println("number is negative");
+    }
+    else if (a>0) {
+      System.out.println("number is positive");
+    }
+    else{
+      System.out.println("number is zero");
+    }
+  }
+  
+}
