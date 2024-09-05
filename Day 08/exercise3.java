15fea4788eef32366a1403afa18d659ba6977f23
Ravitej Mulay <ravitejmulay93@gmail.com>
2024-09-05 22:59:45 +0530

    Day 08 commits

---
+++ Day 08/exercise3.java
@@ -0,0 +1,10 @@
+class exercise3 {
+  public static void main(String[] main){
+    for(int i=1;i<=4;i++){
+      for(int j=1;j<=i;j++){
+        System.out.print("01 ");
+      }
+      System.out.println();
+    }
+  }
+}
---
+++ Day 08/exercise4.java
@@ -0,0 +1,14 @@
+class exercise4 {
+  public static void main(String[] args){
+    for(int i=1;i<=4;i++){
+      for(int j=1;j<=i;j++){
+        if (j==2) {
+          System.out.print("= ");
+        } else {
+          System.out.print("+ ");
+        }
+      }
+      System.out.println();
+    }
+  }
+}
